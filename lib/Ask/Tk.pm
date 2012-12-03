b452e3ac1759f92bed5007122f5f913b45bb4254
Toby Inkster <mail@tobyinkster.co.uk>
2012-12-03 20:12:46 +0000

    document Ask::Tk

--- lib/Ask/Tk.pm
+++ lib/Ask/Tk.pm
@@ -94,3 +94,45 @@ use warnings;
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+Ask::Tk - interact with a user via a GUI
+
+=head1 SYNOPSIS
+
+	my $ask = Ask::Tk->new;
+	
+	$ask->info(text => "I'm Charles Xavier");
+	if ($ask->question(text => "Would you like some breakfast?")) {
+		...
+	}
+
+=head1 BUGS
+
+Please report any bugs to
+L<http://rt.cpan.org/Dist/Display.html?Queue=Ask>.
+
+=head1 SEE ALSO
+
+L<Ask>.
+
+=head1 AUTHOR
+
+Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is copyright (c) 2012 by Toby Inkster.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
